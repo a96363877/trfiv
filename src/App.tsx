41a4abaea16e8b2f1af74c98d33aa2b0d68523af
a96363877 <a96363877@gmail.com>
2025-03-10 22:58:00 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h3> الإدارة العامة للمرور</h3>
+
+      <h1>دولة الكويت</h1>
+      <div className="card" style={{ flexDirection: 'column' }}>
+        <h3> للدخول الموقع يرجى ادخال الرقم الوطني </h3>
+        <input
+          type="tel"
+          maxLength={8}
+          style={{
+            padding: 8,
+            width: '100%',
+            border: '1px #f1f1f1 solid',
+            borderRadius: 5,
+            marginBottom: 5,
+          }}
+        />
+        <a href="#">
+          <button>دخول</button>
+        </a>
+      </div>
+      <p className="read-the-docs">
+        © جميع الحقوق محفوظة لوزارة الداخلية-دولة الكويت - 2025
+      </p>
+    </>
+  );
+}
+
+export default App;
